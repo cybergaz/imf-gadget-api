4a33a3254bd7ef198c161a27ae3a13aa9fc32402
cybergaz <kkanttechy@gmail.com>
2025-02-02 21:49:18 +0530

    small patches

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:latest
 
 WORKDIR /app
 
-COPY package.json ./
+COPY package.json package-lock.json ./
 RUN npm install
 
 COPY . .
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,11 @@ import "dotenv/config"
 export default defineConfig({
     dialect: "postgresql",
     dbCredentials: {
-        host: process.env.POSTGRES_HOST!,
+        host: process.env.POSTGRES_HOST! || "pgdb",
         port: process.env.POSTGRES_PORT! ? parseInt(process.env.POSTGRES_PORT, 10) : 5432,
-        user: process.env.POSTGRES_USER!,
-        password: process.env.POSTGRES_PASSWORD!,
-        database: process.env.POSTGRES_DB!,
+        user: process.env.POSTGRES_USER! || "gaz",
+        password: process.env.POSTGRES_PASSWORD! || "cybergaz",
+        database: process.env.POSTGRES_DB! || "cybergaz",
     },
     schema: ["./src/models/*"]
 })
